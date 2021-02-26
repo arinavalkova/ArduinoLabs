d4012f1df59d9e6e31bbb0381995d157cad576c5
arinavalkova <65497338+arinavalkova@users.noreply.github.com>
2021-02-26 16:16:15 +0700

    Update morseCoder.ino

--- morseCoder.ino
+++ morseCoder.ino
@@ -1,8 +1,6 @@
-#include <stdio.h>
-#include <io.h>
 #define LETTERS_COUNT 26
 #define BITS_SIZE 8
-#define INPUT_BUF_SIZE 128
+#define MAX_INPUT_BUF_SIZE 8
 
 char morseCodes[LETTERS_COUNT] = {
                    (1 << 1) | (1 << 6)
@@ -38,8 +36,8 @@ char letters[LETTERS_COUNT] = {
                     , 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'c', 'q', 'y', 'x'
 };
 
-char findCodeForLetter(char letter) {
-    for (int i = 0; i < LETTERS_COUNT; i++) {
+char findCodeByLetter(char letter) {
+    for (short i = 0; i < LETTERS_COUNT; i++) {
         if (letter == letters[i]) {
             return morseCodes[i];
         }
@@ -66,9 +64,20 @@ int getCountOfMorseDigits(char morseCode) {
     }
 }
 
-char input[INPUT_BUF_SIZE];
+char input[MAX_INPUT_BUF_SIZE];
+short currentInputPos = 0;
+short currentReadPos = 0;
+short currentBufSize = 0;
 
-int main() {
+char currentProcessedLetter;
+char currentProcessedLetterFlag = 0;
+char firstLoop = 1;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
 //    for (int j = 0; j < BITS_SIZE; j++) {
 //        if (codes[i] & 0x80) {
 //            printf("1");
@@ -77,8 +86,51 @@ int main() {
 //        }
 //        codes[i] = codes[i] << 1;
 //    }
-    char letter;
-    read(0, &letter, 1);
-    printf("%d\n", getCountOfMorseDigits(findCodeForLetter(letter)));
-    return 0;
+  if (currentReadPos < currentInputPos && currentProcessedLetterFlag == 0) {
+    currentProcessedLetter = input[currentReadPos];
+    currentProcessedLetter = findCodeByLetter(currentProcessedLetter);
+    currentReadPos++;
+    if (currentReadPos == MAX_INPUT_BUF_SIZE) {
+      currentReadPos = 0;
+    }
+    currentProcessedLetterFlag = 1;
+    currentBufSize--;
+  }
+  
+  if (currentProcessedLetterFlag != 0) {
+     for (int j = 0; j < BITS_SIZE; j++) {
+        if (currentProcessedLetter & 0x80) {
+            Serial.print("1");
+        } else {
+            Serial.print("0");
+        }
+        currentProcessedLetter = currentProcessedLetter << 1;
+    }
+    Serial.println();
+    currentProcessedLetterFlag = 0;
+    //когда закончим работу с символом то сменить флаг
+  }
+  
+  if (currentInputPos == currentReadPos && currentBufSize != 0 && !firstLoop) {
+    return;
+  }
+  
+   if (currentInputPos == MAX_INPUT_BUF_SIZE && currentReadPos == 0) {
+       return;  
+   } else if (currentInputPos == MAX_INPUT_BUF_SIZE) {
+     currentInputPos = 0;
+   }
+    if (Serial.available() > 0) { 
+        input[currentInputPos] = Serial.read();
+ 		currentInputPos++;
+      currentBufSize++;
+      firstLoop = 0;
+    }
+  
+  //for (char i = 0; i < currentInputPos; i++) {
+    //Serial.print(input[i]);
+    //if (i + 1 == currentInputPos) {
+      //Serial.println();
+    //}
+  //}
 }
